b01edb68af45f7463fd6d664dd79272b2f768bb9
jerbaroo <jerbaroo.work@pm.me>
2024-12-06 18:38:47 +0000

    Day 6 part 2 faster

--- src/Advent/Day6.hs
+++ src/Advent/Day6.hs
@@ -3,49 +3,92 @@ module Advent.Day6 where
 import Prelude hiding (Left, Right)
 
 import Advent.Prelude
-import Data.List (find)
+import Data.List (find, nub)
+import Data.Map (Map)
+import Data.Map qualified as Map
 import Data.Set (Set)
 import Data.Set qualified as Set
 
 data Orientation = Up | Down | Left | Right deriving (Eq, Ord, Show)
 type Position    = (Int, Int)
 type Guard       = (Orientation, Position)
-type Crates      = Set Position
 data StopReason  = Loop | OffMap deriving Eq
 
+type Crates'     = Map Int (Set Int)
+-- | We maintain two representations of crates for fast lookup of all the crates
+-- in one row/column, one i indexed first, the other j indexed first.
+type Crates      = (Crates', Crates')
+
+insertCrate :: Position -> Crates -> Crates
+insertCrate (i, j) (iFirst, jFirst) =
+  ( Map.insertWith Set.union i (Set.singleton j) iFirst
+  , Map.insertWith Set.union j (Set.singleton i) jFirst
+  )
+
+isCrate :: Position -> Crates -> Bool
+isCrate (i, j) (m, _) = (Map.lookup i m <&> Set.member j) == Just True
+
 main :: IO ()
-main = print =<< (readMap <&> (uncurry4 part1 &&& uncurry4 part2))
+main = print =<< (readMap <&> (uncurry3 part1 &&& uncurry3 part2))
 
 -- | Count distinct positions guard will visit.
-part1 :: Int -> Int -> Crates -> Guard -> Int
-part1 maxI maxJ crates =
-  Set.size . Set.map snd . snd . walk maxI maxJ Set.empty crates
+part1 :: Position -> Crates -> Guard -> Int
+part1 maxIndices crates =
+  Set.size . Set.fromList . map snd . snd . patrol False maxIndices [] crates
 
 -- | Find positions without loops.
-part2 :: Int -> Int -> Crates -> Guard -> Int
-part2 maxI maxJ crates guard' = do
-  let path = Set.toList $ Set.filter (/= snd guard') $
-        Set.map snd $ snd $ walk maxI maxJ Set.empty crates guard'
-  length $ filter id $ path <&> \newCrate ->
-    isLoop maxI maxJ (Set.insert newCrate crates) guard'
+part2 :: Position -> Crates -> Guard -> Int
+part2 maxIndices crates guard' = do
+  let originalPath = filter (/= snd guard') $ -- Without initial position.
+        nub $ map snd $ snd $ patrol False maxIndices [] crates guard'
+  length $ filter id $ originalPath <&> isLoop
+ where
+  isLoop newCrate = (== Loop) . fst $
+    patrol True maxIndices [] (insertCrate newCrate crates) guard'
 
-isLoop :: Int -> Int -> Crates -> Guard -> Bool
-isLoop maxI maxJ crates = (== Loop) . fst . walk maxI maxJ Set.empty crates
+-- | Patrol until either off the map or a loop detected.
+patrol :: Bool -> Position -> [Guard] -> Crates -> Guard -> (StopReason, [Guard])
+patrol fast maxIndices prevPath crates guard'@(ori, _) = do
+  let path    = guard' : prevPath
+  let nextPos = nextPosition fast maxIndices crates guard'
+  if   outOfBounds maxIndices nextPos then (OffMap, path)
+  else do
+    let nextGuard = avoidCrate crates (ori, nextPos)
+    if   nextGuard `elem` prevPath then (Loop, prevPath)
+    else patrol fast maxIndices path crates nextGuard
 
--- | Walk until wither off the map or a loop detected.
-walk :: Int -> Int -> Set Guard -> Crates -> Guard -> (StopReason, Set Guard)
-walk maxI maxJ seen crates guard'@(ori, pos) =
-  case forwardMay maxI maxJ guard' of
-    Nothing         -> (OffMap, Set.insert guard' seen) -- Walked off the map.
-    Just forwardPos -> do                               -- Still on the map.
-      let crateBlocking = forwardPos `Set.member` crates
-      let nextGuard =
-            if   crateBlocking
-            then (turnRight ori, pos       ) -- Either change orientation..
-            else (ori          , forwardPos) -- ..or move forward.
-      if   nextGuard `elem` seen && not crateBlocking
-      then (Loop, seen)                                             -- Loop!
-      else walk maxI maxJ (Set.insert guard' seen) crates nextGuard -- No loop.
+avoidCrate :: Crates -> Guard -> Guard
+avoidCrate crates (ori, pos) =
+  if isCrate pos crates then (turnRight ori, stepBack (ori, pos)) else (ori, pos)
+
+outOfBounds :: Position -> Position -> Bool
+outOfBounds (maxI, maxJ) (i, j) = i < 0 || j < 0 || i > maxI || j > maxJ
+
+nextPosition :: Bool -> Position -> Crates -> Guard -> Position
+nextPosition fast maxIndices crates =
+  if fast then stepForwardFast maxIndices crates else stepForward
+
+stepBack :: Guard -> Position
+stepBack (Up   , (i, j)) = (i+1, j  )
+stepBack (Down , (i, j)) = (i-1, j  )
+stepBack (Left , (i, j)) = (i  , j+1)
+stepBack (Right, (i, j)) = (i  , j-1)
+
+stepForward :: Guard -> Position
+stepForward (Up   , (i, j)) = (i-1, j  )
+stepForward (Down , (i, j)) = (i+1, j  )
+stepForward (Left , (i, j)) = (i  , j-1)
+stepForward (Right, (i, j)) = (i  , j+1)
+
+stepForwardFast :: Position -> Crates -> Guard -> Position
+stepForwardFast (maxI, maxJ) (iFirst, jFirst)  (ori, (i, j)) = f ori
+ where
+  f Up    = upDown (-1)          Set.lookupLT
+  f Down  = upDown (maxI + 1)    Set.lookupGT
+  f Left  = leftRight (-1)       Set.lookupLT
+  f Right = leftRight (maxJ + 1) Set.lookupGT
+  leftRight def lookup' = (i,) $ fromMaybe def $ lookup' j =<< Map.lookup i iFirst
+  upDown    def lookup' = (,j) $ fromMaybe def $ lookup' i =<< Map.lookup j jFirst
 
 turnRight :: Orientation -> Orientation
 turnRight Up    = Right
@@ -53,36 +96,19 @@ turnRight Down  = Left
 turnRight Left  = Up
 turnRight Right = Down
 
-forwardMay :: Int -> Int -> Guard -> Maybe Position
-forwardMay maxI maxJ guard' =
-  let pos@(i, j) = forward guard'
-  in  if i < 0 || j < 0 || i > maxI || j > maxJ then Nothing else Just pos
-
-forward :: Guard -> Position
-forward (Up   , (i, j)) = (i-1, j  )
-forward (Down , (i, j)) = (i+1, j  )
-forward (Left , (i, j)) = (i  , j-1)
-forward (Right, (i, j)) = (i  , j+1)
-
 -- | Read orientation and position of guard, and positions of crates
-readMap :: IO (Int, Int, Crates, Guard)
+readMap :: IO (Position, Crates, Guard)
 readMap = readFile "data/Day6.txt" <&> \string -> do
-  let lines' = lines string
-  let cells  :: [(Position, Char)] = concat
-        [ [ ((i, j), char) | (j, char) <- zip [0 .. ] line ]
-        | (i, line) <- zip [0 .. ] lines'
-        ]
-  let guardCell = fromMaybe (error "Can't find guard") $
+  let rows = lines string
+  let cells = concat [ zip ((i,) <$> [0 .. ]) row | (i, row) <- zip [0 .. ] rows ]
+  let crateCells = map fst $ filter ((== '#') . snd) cells
+  let (guardPos, guardChar) = fromMaybe (error "Can't find guard") $
         find (\(_, c) -> c /= '.' && c /= '#') cells
-  let guardOrientation =
-        case snd guardCell of
-          '^' -> Up
-          '>' -> Right
-          'v' -> Down
-          '<' -> Left
-          c   -> error $ "Player orientation not recognised: '" <> [c] <> "'"
-  (   length lines' - 1
-    , length (head lines') - 1
-    , Set.fromList [ fst cell | cell <- cells, snd cell == '#' ]
-    , (guardOrientation, fst guardCell)
+  let guardOrientation = case guardChar of
+        '^' -> Up; '>' -> Right; 'v' -> Down; '<' -> Left;
+        c   -> error $ "Player orientation not recognised: '" <> [c] <> "'"
+  let crates f g = foldl' (\m x -> Map.insertWith Set.union (f x) (Set.singleton $ g x) m) Map.empty crateCells
+  ( ( length rows - 1, length (head rows) - 1 )
+    , ( crates fst snd, crates snd fst )
+    , (guardOrientation, guardPos)
     )
--- src/Advent/Prelude.hs
+++ src/Advent/Prelude.hs
@@ -23,5 +23,8 @@ boolToMaybe b x = if b then Just x else Nothing
 debug :: Show a => a -> a
 debug a = trace (show a) a
 
+uncurry3 :: (a -> b -> c -> d) -> ((a, b, c) -> d)
+uncurry3 f (a, b, c) = f a b c
+
 uncurry4 :: (a -> b -> c -> d -> e) -> ((a, b, c, d) -> e)
 uncurry4 f (a, b, c, d) = f a b c d
